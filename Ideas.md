d018b3a9c09e1cdef0d829a177d458eedd5dcad8
AleenaCodes <AleenaCodes@users.noreply.github.com>
2019-01-05 23:36:20 +0000

    Initial files - tested out parsing and stripping

---
+++ Ideas.md
@@ -0,0 +1,17 @@
+# Ideas
+
+
+Have in a struct -
+
+Name        : Str : Name of Spell
+Total       : Int : Total number of mentions
+Occurences  : []  : Line number of each occurrence
+Type        : Str : Category of spell
+
+
+Could splice together all txt files and then run
+If statement to just get each spell out? Import as set and check if in
+Find a list of all spells!
+
+- Keep a note which lines each book start and end on
+- Visualisation is just a matter of making dot on percentage of total number of lines
---
+++ numWords.py
@@ -0,0 +1,35 @@
+import re
+
+inputFile = open("Books/Book1.txt.", 'r', encoding='utf8')
+outputFile = open("Books/Book1Stripped.txt", 'w', encoding='utf8')
+
+line = inputFile.readline()
+
+while line:
+    outputLine = re.sub('[^a-zA-Z0-9\n]', ' ', line)
+    outputFile.write(outputLine)
+    line = inputFile.readline()
+
+
+wordsDict = {}
+
+inputFile = open("Books/Book1Stripped.txt", 'r', encoding='utf8')
+
+line = inputFile.readline()
+
+while line:
+    for word in line.split():
+        wordsDict[word] = wordsDict.get(word,0) + 1
+    line = inputFile.readline()
+
+print("========finished first loop===========")
+
+iter = 0
+
+for key in sorted(wordsDict, key=wordsDict.get, reverse=True):
+  print("{} : {}".format(key,wordsDict[key]))
+  iter += 1
+  if (iter>20):
+      break
+
+# print("num of Wingardium is " + str(wordsDict["Wingardium"]))
